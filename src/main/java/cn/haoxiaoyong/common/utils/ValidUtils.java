8da0d7880d31b89e805eea4125f78cb693c2bd6a
haoxiaoyong <hxyHelloWorld@163.com>
2020-03-18 16:57:53 +0800

    :heavy_plus_sign: Add Slf4j

--- src/main/java/cn/haoxiaoyong/common/utils/ValidUtils.java
+++ src/main/java/cn/haoxiaoyong/common/utils/ValidUtils.java
@@ -2,6 +2,7 @@ package cn.haoxiaoyong.common.utils;
 
 import cn.haoxiaoyong.common.enums.CommonResultEnum;
 import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Created by haoxiaoyong on 2020/3/3 上午 9:15
@@ -11,6 +12,7 @@ import com.alibaba.fastjson.JSONObject;
  * <p>
  * 检查一个对象中的参数值是否为空
  */
+@Slf4j
 public class ValidUtils {
 
     public static <T> JSONObject inspect(T t, String requireParams) {
@@ -20,6 +22,7 @@ public class ValidUtils {
         JSONObject jsonObject = JSONObject.parseObject(jsonString);
         String[] params = requireParams.split(",");
         for (String param : params) {
+        log.info("patams: {}", param);
             if (jsonObject.getString(param) == null || jsonObject.getString(param).isEmpty()) {
                 jsonResult.put("code", CommonResultEnum.CHECK_VALID_ERROR.code());
                 jsonResult.put("msg", String.format(CommonResultEnum.CHECK_VALID_ERROR.message(), param));
